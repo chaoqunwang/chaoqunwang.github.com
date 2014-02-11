50514de0cbc9b285b1b5768a7280d5a833a3bc89
colalife <colalife@hotmail.com>
2014-02-11 14:53:49 +0800

    init

--- plugins/post_footer_filter.rb
+++ plugins/post_footer_filter.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 #
 # post_footer_filter.rb
 # Append every post some footer infomation like original url 
@@ -11,12 +12,16 @@ module AppendFooterFilter
      author = post.site.config['author']
      url = post.site.config['url']
      pre = post.site.config['original_url_pre']
-     post.content + %Q[<p class='post-footer'>
-            #{pre or "original link:"}
-            <a href='#{post.full_url}'>#{post.full_url}</a><br/>
-            &nbsp;written by &nbsp;<a href='#{url}'>#{author}</a>
-            &nbsp;posted at &nbsp;<a href='#{url}'>#{url}</a>
-            </p>]
+#     post.content + %Q[<p class='post-footer'>
+#            #{pre or 'original link:'}
+#            <a href='#{post.full_url}'>#{post.full_url}</a><br/>
+#            &nbsp;written by &nbsp;<a href='#{url}'>#{author}</a> &nbsp;posted at &nbsp;<a href='#{url}'>#{url}</a>
+#            </p>]
+	post.content + %Q[<p class='post-footer' style='background:none repeat scroll 0pt 0pt #eee;padding: 5px;margin-top:10px;color:gray;font-size:90%;'>
+			&nbsp;#{pre or 'original link:'}
+			<a href='#{post.full_url}'>#{post.full_url}</a><br/>
+			&nbsp;written by &nbsp;<a href='#{url}'>#{author}</a> &nbsp;posted at &nbsp;<a href='#{url}'>#{url}</a><br/>
+			&nbsp;版权声明：自由转载-非商用-非衍生-保持署名 | <a href='http://creativecommons.org/licenses/by-nc-nd/3.0/deed.zh'>Creative Commons BY-NC-ND 3.0</a></p>]
   end 
 end
 
