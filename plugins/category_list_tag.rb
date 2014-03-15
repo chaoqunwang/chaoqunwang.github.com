9de85578cde12784d7070fc47759bb93b5703a51
colalife <colalife@hotmail.com>
2014-03-15 13:42:15 +0800

    change title size, categories, links

--- plugins/category_list_tag.rb
+++ plugins/category_list_tag.rb
@@ -3,11 +3,14 @@ module Jekyll
     def render(context) 
       html = "" 
       categories = context.registers[:site].categories.keys 
-      categories.sort.each do |category| 
+      categories.sort.each_with_index do |category, i| 
+		tag_before, tag_after = '', ''
+		tag_before = "<li>" if i%3 == 0
+		tag_after = "</li>" if i%3 == 2
         posts_in_category = context.registers[:site].categories[category].size 
         category_dir = context.registers[:site].config['category_dir']
         category_url = File.join(category_dir, category.gsub(/_|\P{Word}/u, '-').gsub(/-{2,}/u, '-').downcase.to_url) 
-        html << "<li class='category'><a href='/#{category_url}/'>#{category} (#{posts_in_category})</a></li>\n" 
+        html << "#{tag_before}<a style=\"padding: 3px;\" href='/#{category_url}/'>#{category} (#{posts_in_category})</a>#{tag_after}\n" 
       end 
       html 
     end 
